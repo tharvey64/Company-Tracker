97e5d97ff06591ead7641b6827ad3f0ea41f29b9
Thomas Harvey <thomasharvey24@gmail.com>
2015-05-04 16:43:41 -0400

    Basic graph with quandl data. Markit app creates live graph. Still need to work on event listeners

--- tracker_project/tracker_project/urls.py
+++ tracker_project/tracker_project/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^markit/', include('markit.urls', namespace='markit')),
+    url(r'^quandl/', include('quandl.urls', namespace='quandl')),
     url(r'^admin/', include(admin.site.urls)),
 ]
