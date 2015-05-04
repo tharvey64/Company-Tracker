00f288f46f27e8cb704137a8f7775da2fe5b7799
Anthony Keating <anthonyk1225@gmail.com>
2015-05-04 18:08:56 -0400

    added twitter app

--- tracker_project/tracker_project/urls.py
+++ tracker_project/tracker_project/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^markit/', include('markit.urls', namespace='markit')),
     url(r'^quandl/', include('quandl.urls', namespace='quandl')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'stockgraph/', include('stockgraph.urls')),
+    url(r'^stockgraph/', include('stockgraph.urls')),
+    url(r'^twitter/', include('twitter.urls')),
 ]
