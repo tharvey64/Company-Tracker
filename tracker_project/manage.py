ddfc70d9d377e40fa32783e3965efdb076f1771e
Anthony Keating <anthonyk1225@gmail.com>
2015-05-04 17:01:04 -0400

    added markit data w/ form on stockgraph app

--- tracker_project/manage.py
+++ tracker_project/manage.py
@@ -3,11 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracker_project.settings")
-=======
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracker.settings")
->>>>>>> master
+
 
     from django.core.management import execute_from_command_line
 
