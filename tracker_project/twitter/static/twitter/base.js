712cbf3a4e463b27ce19051bd0b1b4fbdd2aadd4
Anthony Keating <anthonyk1225@gmail.com>
2015-05-06 18:14:47 -0400

    switching y axis and z axis

--- tracker_project/twitter/static/twitter/base.js
+++ tracker_project/twitter/static/twitter/base.js
@@ -1,27 +1,32 @@
 $(document).ready(function(){    
     $('#searchTwitter').on('submit', function(event){
         event.preventDefault();
+        $('#graph').empty()
         $.post($(this).attr('action'),
             {'search': ($('#search')).val(), csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value, 'type' :$('[name=filter]').val() },
             function(data) {
                 var dataset = []
                 var dates = data['dates'] 
                 var favorites = data['favorites']
+                var tweets = data['tweets']
                 var feelings = data['feelings']
                 for (i=0; i < dates.length; i++) {
                     if (feelings[i]['feeling']['docSentiment'] != undefined){
-                        dataset.push([dates[i]['date'], favorites[i]['favorite'], feelings[i]['feeling']['docSentiment']['score']])
+                        dataset.push([dates[i]['date'], favorites[i]['favorite'], tweets[i]['content'], feelings[i]['feeling']['docSentiment']['score']])
                     }
                     else{
                         continue
                     }
                 } 
-            var padding = 30;
+            var padding = 60;
+            var high = d3.max(dataset,function(d){return d[1]});            
+            var verticalTicks = high/10;
             var graphHeight = parseInt($("#graph").css('height'));
             var h = graphHeight
             var graphWidth = parseInt($("#graph").css('width'));
             var w = graphWidth; 
             var parseDate = d3.time.format("%a %B %e %X %Z %Y").parse; 
+            var horizontalTicks = w/100;                            
 
             var yScale = d3.scale.linear()
                 .domain([0, d3.max(dataset,function(d){return d[1]})])
@@ -32,6 +37,14 @@ $(document).ready(function(){
                     return parseDate(d[0]);
                 }))
                 .range([padding, w - padding]);
+
+            var xAxis = d3.svg.axis();
+            xAxis.scale(xScale).orient("bottom");
+            xAxis.ticks(horizontalTicks);   
+
+            var yAxis = d3.svg.axis();
+            yAxis.scale(yScale).orient("left");
+            yAxis.ticks(10);
 
             var svg = d3.select("#graph")
                         .append("svg")
@@ -45,15 +58,24 @@ $(document).ready(function(){
                     return xScale(parseDate(d[0]));
                 })
                 .attr("cy", function(d){
-                    console.log(h)
-                    return yScale(d[1])
+                    return yScale(d[3])
                 })
                 .attr("r", function(d) {
-                    return d[1] / 1000;
+                    return 10;
                 })
-                .attr("fill", function(d) {
-                    return d3.rgb((d[2] * -255),(d[2] * 255),0);
-                });
+                .attr("fill", "gold")
+                // .attr("fill", function(d) {
+                //     return d3.rgb((d[2] * -255),(d[2] * 255),0);
+                // });
+                svg.append("g")
+                    .attr("class", "axis")
+                    .attr("transform", "translate(0," + (h - padding) + ")")
+                    .call(xAxis);
+                svg.append("g")
+                    .attr("class", "axis")
+                    .attr("transform", "translate(" + padding +",0)")
+                    .call(yAxis);
+                $('svg > circle').tooltips();
         //         var template = $('#sentimentTemplate').html();
         //         Mustache.parse(template);
         //         var info = Mustache.render(template, data);
@@ -62,10 +84,10 @@ $(document).ready(function(){
         //         Mustache.parse(template);
         //         var info = Mustache.render(template, data);
         //         $('#postedTweets').html(info); 
-        //         var template = $('#hashTemplate').html();
-        //         Mustache.parse(template);
-        //         var info = Mustache.render(template, data);
-        //         $('#postedHashes').html(info);                                 
+                var template = $('#hashTemplate').html();
+                Mustache.parse(template);
+                var info = Mustache.render(template, data);
+                $('#postedHashes').html(info);                                 
             }
         )
     
--- tracker_project/twitter/views.py
+++ tracker_project/twitter/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from django.http import JsonResponse
 from twython import Twython
 from alchemyapi import AlchemyAPI
+from collections import Counter
 from tracker_project.settings import TWITTER_KEY
 from tracker_project.settings import TWITTER_SECRET
 
@@ -17,17 +18,19 @@ class SearchView(View):
     def post(self, request):
         tweets = []
         twitter = Twython(TWITTER_KEY, TWITTER_SECRET)
-        result = twitter.search(q=request.POST['search'], count=5, result_type=request.POST['type'], lang='en') #everything
+        result = twitter.search(q=request.POST['search'], count=2, result_type=request.POST['type'], lang='en') #everything
         start_point = result['statuses'] #one level into result... shouldnt have to use result again
         for tweet in start_point:
             if tweet['user']['followers_count'] > 5000:
                 tweets.append(tweet['text']) # if user has more than 5k followers, we keep thier tweet
-        response = self.alchemyapi.sentiment("text", ('').join(actual_words))
-        feeling = response.get("docSentiment")
-        if feeling == None:
-            return JsonResponse({'result' : 'There was a problem searching for ' + request.POST['search'] + '. Please try a different filter.'})
+        broken_down = [line['entities'] for line in start_point] #hashtags 1
+        almost_there = [block['hashtags'] for block in broken_down] #hashtags 2
+        hashtags = [last[0]['text'].lower() for last in almost_there if len(last) > 0] #hashtags final
+        hash_dict = Counter(hashtags)
+        top_hashes = hash_dict.most_common(5)        
         return JsonResponse({'dates' : [{'date' : date['created_at']} for date in start_point], 
                             'feelings' : [{'feeling' : self.alchemyapi.sentiment("text", block)} for block in tweets], 
-                            'favorites' : [{'favorite' : favorite['favorite_count']} for favorite in start_point]})
-
+                            'favorites' : [{'favorite' : favorite['favorite_count']} for favorite in start_point],
+                             'hashes': [{'name': hashe[0]} for hashe in top_hashes],
+                             'tweets' : [{'content' : line} for line in tweets]})
 
