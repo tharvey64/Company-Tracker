98491202e4cf2357c2ac5bb9f77b37d4d6a2b9ab
Thomas Harvey <thomasharvey24@gmail.com>
2015-05-12 17:40:00 -0400

    adds sentiment button to articles

--- tracker_project/article_feeds/views.py
+++ tracker_project/article_feeds/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.generic import View
 import feedparser
+from alchemyapi import AlchemyAPI
 
 
 # Create your views here.
@@ -13,3 +14,12 @@ class YahooArticleListView(View):
         feed_response = feedparser.parse(self.site + symbol)
         entries = feed_response['entries']
         return JsonResponse({'articles': entries})
+
+class ArticleSentiment(View):
+    alchemy = AlchemyAPI()
+
+    def get(self, request):
+        url = self.request.GET.get('url', False)
+        if url:
+            return JsonResponse(self.alchemy.sentiment('url', url))
+        return JsonResponse({'error': 'Missing Url'})
