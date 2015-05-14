176fe94be31918fcd6fd2a11168b507fe73e0884
Anthony Keating <anthonyk1225@gmail.com>
2015-05-14 11:35:39 -0400

    added login/register forms for twitter lists & oauth. certain divs now show when they're supposed to

--- tracker_project/static/article_feeds/story.js
+++ tracker_project/static/article_feeds/story.js
@@ -1,17 +1,8 @@
 $(document).ready(function(){
-    $("#tab1, #graph").on("submit", "#stockForm, .company-list form", function(event){
+    $("#stories").on("getStories", function(event, symbol){
         event.preventDefault();
-        var tag = $(this).attr("id"),
-        symbol;
-        if (tag == "stockForm"){
-            symbol = $("input[name='company_symbol']").val();
-        }
-        else{
-            symbol = $($("#"+tag+" button ul li input")[1]).val();
-        }
         $.getJSON("/article_feeds/yahoo/" + symbol + "/", function(data){
             var template = $('#story-template').html();
-            console.log()
             var info = Mustache.render(template, data);
             $("#stories").html(info);       
         });
