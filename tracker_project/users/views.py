3ad584ce9841fd1743b1270a45cef74eb2a3dcb6
Anthony Keating <anthonyk1225@gmail.com>
2015-05-15 18:44:34 -0400

    registrations complete. after signing up with oauth, you may finish your registrations process

--- tracker_project/users/views.py
+++ tracker_project/users/views.py
@@ -2,22 +2,27 @@ from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
+from twitter.models import Profile
 # Create your views here.
 
 class RegisterView(View):
+    template = 'users/register_form.html'
 
-    def post(request):
-        username = request.POST.get('username',False)
-        email = request.POST.get('email',None)
-        password = request.POST.get('password',False)
+    def get(self, request):
+        return render(request, self.template)
+
+    def post(self, request):
+        username = request.POST.get('registerUsername',False)
+        email = request.POST.get('registerEmail',None)
+        password = request.POST.get('registerPassword',False)
         if username and password:
-            user = User.objects.create_user(username,email,password)
-            # redirect('pages:success', status='Registered.')
-        # redirect('pages:error', error='Invalid username or password')
+            user = User.objects.create_user(username, email, password)
+            profile = Profile.objects.create(token=request.session['OAUTH_TOKEN'], secret=request.session['OAUTH_TOKEN_SECRET'], user=user)
+        return redirect('/quandl')
 
 class LoginView(View):
 
-    def post(request):
+    def post(self, request):
         username = request.POST.get('username', False)
         password = request.POST.get('password', False)
         user = authenticate(username=username,password=password)
@@ -31,7 +36,7 @@ class LoginView(View):
 
 class LogoutView(View):
 
-    def post(request):
+    def post(self, request):
         logout(request)
         return redirect('pages:success', status='Logged out.')
 
