21fabe420bb6df6ecca2037cd97cffecfee2a211
Anthony Keating <anthonyk1225@gmail.com>
2015-05-17 19:58:07 -0400

    repositioned graphing page

--- tracker_project/static/quandl/query.js
+++ tracker_project/static/quandl/query.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     $("#tab2").on("submit", "#searchForm", function(event) {
         $("#loading").css("display", "block");          
-        setTimeout(function(){         
+        setTimeout(function(){      
+        $("#logout").css("display", "none");
         $("#loading").css("display", "none");
         $("#backButton").css("display", "block")       
         $("#graph").css("display", "inline-block"); 
@@ -13,6 +14,7 @@ $(document).ready(function(){
     $("#tab1").on("submit", "#stockForm", function(event) {
             $("#loading").css("display", "block");          
         setTimeout(function(){ 
+            $("#logout").css("display", "none");
             $("#loading").css("display", "none");
             $("#backButton").css("display", "block")       
             $("#graph").css("display", "inline-block"); 
@@ -25,6 +27,7 @@ $(document).ready(function(){
     $("#twitterForm").on("submit", function(event) {
             $("#loading").css("display", "block");          
         setTimeout(function(){ 
+            $("#logout").css("display", "none");
             $("#loading").css("display", "none");
             $("#backButton").css("display", "block");
             $("#results").css("display", "block"); 
@@ -37,14 +40,16 @@ $(document).ready(function(){
         $("#backButton").css("display", "none");       
         $(".returned").css("display", "none");  
         $("#results").css("display", "none"); 
+        $("#logout").css("display", "block");
         $("#stories, #graph").empty();
         $("h1").slideDown(1500); 
         $("#forms").slideDown(1500);    
     }); 
     $("#graph").on("click", "button.search-result", function(event){
-        $("#backButton").css("display", "none");       
+        $("#backButton").css("display", "none");  
         $(".returned").css("display", "none");  
         $("#results").css("display", "none"); 
+        $("#logout").css("display", "block");
         $("h1").slideDown(1500); 
         $("#forms").slideDown(1500); 
 
