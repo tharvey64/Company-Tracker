e4dd90bd0442d64608e86a044d29de1181f836f1
Thomas Harvey <thomasharvey24@gmail.com>
2015-05-18 13:04:45 -0400

    Split twitter search into 2 views

--- tracker_project/twitter/urls.py
+++ tracker_project/twitter/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 import twitter.views as view
 
 urlpatterns = [
-    url(r'^search/$', view.SearchView.as_view(), name ='postsearch'),
+    url(r'^search/random$', view.SearchView.as_view(), name ='postsearch'),
+    url(r'^search/lists$', view.SearchListView.as_view(), name ='listsearch'),
     url(r'^oauth/$', view.AppView.as_view(), name='oauth'),
 	url(r'^callback/$', view.CallbackView.as_view(), name ='index')    
 ]
--- tracker_project/twitter/views.py
+++ tracker_project/twitter/views.py
@@ -33,73 +33,75 @@ class SearchView(View):
     alchemyapi = AlchemyAPI()
 
     def post(self, request):
-        print(request.POST)
         user_query = request.POST['search']   #the user searched for this  
-        if request.POST['path'] == 'random':
 
-            twitter = Twython(TWITTER_KEY, TWITTER_SECRET)
+        twitter = Twython(TWITTER_KEY, TWITTER_SECRET)
 
-            twython_results = twitter.search(q=user_query, result_type=request.POST.get('type',False), lang='en') #twitter search results
+        twython_results = twitter.search(q=user_query, result_type=request.POST.get('type',False), lang='en') #twitter search results
+        
+        keyword, created = Keyword.objects.get_or_create(search__iexact=user_query)
+
+        stored_tweets_of_query = keyword.tweet.all()#tweets in the database
+
+        new_tweets = []
+
+        for response in twython_results['statuses']: #iterating through each tweet
+
+            old_tweet = stored_tweets_of_query.filter(tweet_id=response['id_str'])
             
-            keyword, created = Keyword.objects.get_or_create(search__iexact=user_query)
+            if old_tweet and len(old_tweet) == 1:
+                old_tweet[0].favorites = response['favorite_count']
+                old_tweet[0].save()
+            else:
+                alchemy_result = self.alchemyapi.sentiment('text', response['text'])  
+                if alchemy_result.get('status', False) != 'OK':
+                    continue
 
-            stored_tweets_of_query = keyword.tweet.all()#tweets in the database
+                tweet_sentiment_value = Sentiment.objects.create(
+                    score=alchemy_result['docSentiment'].get('score', 0), 
+                    value=alchemy_result['docSentiment']['type']
+                )   
 
-            new_tweets = []
+                formatted_date = datetime.datetime.strptime(response['created_at'], "%a %B %d %X %z %Y")
 
-            for response in twython_results['statuses']: #iterating through each tweet
+                tweet = Tweet.objects.create(
+                    text=response['text'], 
+                    tweet_id=response['id_str'], 
+                    favorites=response['favorite_count'],
+                    tweet_date=formatted_date, 
+                    sentiment=tweet_sentiment_value
+                )
+                new_tweets.append(tweet)
+        
+        keyword.tweet.add(new_tweets)
 
-                old_tweet = stored_tweets_of_query.filter(tweet_id=response['id_str'])
-                
-                if old_tweet and len(old_tweet) == 1:
-                    old_tweet[0].favorites = response['favorite_count']
-                    old_tweet[0].save()
-                else:
-                    alchemy_result = self.alchemyapi.sentiment('text', response['text'])  
-                    if alchemy_result.get('status', False) != 'OK':
-                        continue
+        all_tweets = new_tweets + stored_tweets_of_query
 
-                    tweet_sentiment_value = Sentiment.objects.create(
-                        score=alchemy_result['docSentiment'].get('score', 0), 
-                        value=alchemy_result['docSentiment']['type']
-                    )   
+        tweet_dataset = [[row.tweet_date.strftime("%Y-%m-%d %H:%M:%S%z"), row.sentiment.score, row.favorites, row.text] for row in all_tweets]
+        return JsonResponse({'tweets': tweet_dataset})
 
-                    formatted_date = datetime.datetime.strptime(response['created_at'], "%a %B %d %X %z %Y")
+class SearchListView(View):
+    alchemyapi = AlchemyAPI()
 
-                    tweet = Tweet.objects.create(
-                        text=response['text'], 
-                        tweet_id=response['id_str'], 
-                        favorites=response['favorite_count'],
-                        tweet_date=formatted_date, 
-                        sentiment=tweet_sentiment_value
-                    )
-                    new_tweets.append(tweet)
-            
-            keyword.tweet.add(new_tweets)
+    def post(self, request):
+        user_query = request.POST['search'] 
+        profile = Profile.objects.filter(user__pk=request.user.id)
+        twitter = Twython(TWITTER_KEY, TWITTER_SECRET, profile[0].token, profile[0].secret)
+        list_of_tweets = twitter.get_list_statuses(slug=request.POST['listName'], owner_screen_name=request.user.username, count=200)
+        list_dataset = []
+        for unique_tweet in list_of_tweets:
+            if user_query.lower() in unique_tweet['text'].lower():
 
-            all_tweets = new_tweets + stored_tweets_of_query
+                alchemy_result = self.alchemyapi.sentiment("text", unique_tweet['text'])
+                if not alchemy_result.get('docSentiment', False):
+                    continue
+                unique_tweet['sentiment'] = alchemy_result['docSentiment'].get('score', 0)
+                unique_tweet['created_at'] = datetime.datetime.strptime(unique_tweet['created_at'], "%a %B %d %X %z %Y")
 
-            tweet_dataset = [[row.tweet_date.strftime("%Y-%m-%d %H:%M:%S%z"), row.sentiment.score, row.favorites, row.text] for row in all_tweets]
-            return JsonResponse({'tweets': tweet_dataset})
-
-        else:
-            profile = Profile.objects.filter(user__pk=request.user.id)
-            twitter = Twython(TWITTER_KEY, TWITTER_SECRET, profile[0].token, profile[0].secret)
-            list_of_tweets = twitter.get_list_statuses(slug=request.POST['listName'], owner_screen_name=request.user.username, count=200)
-            list_dataset = []
-            for unique_tweet in list_of_tweets:
-                if user_query.lower() in unique_tweet['text'].lower():
-
-                    alchemy_result = self.alchemyapi.sentiment("text", unique_tweet['text'])
-                    if not alchemy_result.get('docSentiment', False):
-                        continue
-                    unique_tweet['sentiment'] = alchemy_result['docSentiment'].get('score', 0)
-                    unique_tweet['created_at'] = datetime.datetime.strptime(unique_tweet['created_at'], "%a %B %d %X %z %Y")
-
-                    list_dataset.append([
-                        unique_tweet['created_at'].strftime("%Y-%m-%d %H:%M:%S%z"), 
-                        unique_tweet['sentiment'], 
-                        unique_tweet['favorite_count'], 
-                        unique_tweet['text']
-                    ])
-            return JsonResponse({'tweets': list_dataset})
+                list_dataset.append([
+                    unique_tweet['created_at'].strftime("%Y-%m-%d %H:%M:%S%z"), 
+                    unique_tweet['sentiment'], 
+                    unique_tweet['favorite_count'], 
+                    unique_tweet['text']
+                ])
+        return JsonResponse({'tweets': list_dataset})
