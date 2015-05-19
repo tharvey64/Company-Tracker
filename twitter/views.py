294c88d99bfae34fcd0c0eefc8411c21a426f9c6
Anthony Keating <anthonyk1225@gmail.com>
2015-05-19 17:56:25 -0400

    twitter is really fixed now

--- twitter/views.py
+++ twitter/views.py
@@ -82,7 +82,7 @@ class SearchListView(View):
         user_query = request.POST['search'] 
         profile = Profile.objects.filter(user__pk=request.user.id)
         twitter = Twython(TWITTER_KEY, TWITTER_SECRET, profile[0].token, profile[0].secret)
-        list_of_tweets = twitter.get_list_statuses(slug=request.POST['list_name'], owner_screen_name=request.user.username, count=200)
+        list_of_tweets = twitter.get_list_statuses(slug=request.POST['listName'], owner_screen_name=request.user.username, count=200)
         list_dataset = []
         for unique_tweet in list_of_tweets:
             if user_query.lower() in unique_tweet['text'].lower():
