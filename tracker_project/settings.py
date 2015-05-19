97be3374d7058752d6a4c35bcfc47be687439884
Thomas Harvey <thomasharvey24@gmail.com>
2015-05-19 13:57:02 -0400

    adds pw hashers

--- tracker_project/settings.py
+++ tracker_project/settings.py
@@ -63,6 +63,16 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+)
+
 ROOT_URLCONF = 'tracker_project.urls'
 
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
