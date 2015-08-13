f594af5e3dd0ff6aafe2917ebcce0b4aa11fa5b2
Thomas Harvey <thomasharvey24@gmail.com>
2015-08-13 12:13:52 -0400

    moved settings files

--- tracker_project/wsgi.py
+++ tracker_project/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 # from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracker_project.settings_local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracker_project.settings.local")
 application = get_wsgi_application()
 # application = Cling(get_wsgi_application())
